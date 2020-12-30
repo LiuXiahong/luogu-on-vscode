3cd3dc8b5c7be15743f9b73cf5bc6989b776fb90
LiuXiahong <l15178725046@163.com>
2020-12-30 14:10:24 +0800

    demo 9

--- extension.js
+++ extension.js
@@ -8,17 +8,24 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-
-function sleep (time) {
-	return new Promise((resolve) => setTimeout(resolve, time));
-  }  
-
+// const request = require('request');
+const request = require('request-promise');
+async function getLuoguApi(url) {
+	let options = {
+		method: 'GET',
+		uri: url
+	};
+	let data = await request(options);
+	data = JSON.parse(data);
+	// console.log("I got data", data);
+	return Promise.resolve(data);
+}
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "luogu-on-vscode" is now active!');
-	const request = require('request');
+
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
@@ -27,47 +34,66 @@ function activate(context) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from luogu-on-vscode!');
+		// var a = {};
+		// getLuoguApi('https://www.luogu.com.cn/ranking?_contentOnly').then(result => {
+		// 	a = result
+		// 	// console.log(result) // 这里拿到返回值
+		// });
+		// setTimeout(function () { console.log(a) }, 3000);
 	});
 	let showranking = vscode.commands.registerCommand('luogu-on-vscode.showranking', function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		// var a = [{
-		// 	label: 'first',
-		// 	description: 'first item',
-		// 	detail: 'first item details'
-		// }, {
-		// 	label: 'second',
-		// 	description: 'second item',
-		// 	detail: 'second item details'
-		// }];
+		// var arr = [];
+		// var url = 'https://www.luogu.com.cn/ranking?_contentOnly';
+		// var body = getLuoguApi(url);
+		// console.log(body);
+		// var ranking = body.currentData.rankList.result;
+		// for (var i = 0, len = ranking.length; i < len; i++) {
+		// 	var t = {};
+		// 	t.label = ranking[i].user.name;
+		// 	t.description = ranking[i].user.slogan;
+		// 	t.detail = String(ranking[i].contestRating + ' ' + ranking[i].socialRating + ' ' + ranking[i].practiceRating + ' ' + ranking[i].basicRating + ' ' + ranking[i].prizeRating);
+		// 	arr.push(t);
+		// }
+		// console.log(arr.length);
+		// vscode.window.showQuickPick(arr, {
+		// 	canPickMany: false,
+		// 	ignoreFocusOut: true,
+		// 	matchOnDescription: true,
+		// 	matchOnDetail: true,
+		// 	placeHolder: body.currentTitle
+		// }).then(value => {
+		// 	vscode.window.showInformationMessage("看看人家" + value.label + ",再看看你自己！");
+		// })
 		var arr = [];
-		url = 'https://www.luogu.com.cn/ranking?_contentOnly';
-		request(url, { json: true }, (err, res, body) => {
-			if (err) {
-				return console.log(err);
-			}
-			console.log(body);
-			var ranking = body.currentData.rankList.result;
-			for (var i = 0, len = ranking.length; i < len; i++) {
-				var t = {};
-				t.label = ranking[i].user.name;
-				t.description = ranking[i].user.slogan;
-				t.detail = String(ranking[i].contestRating+' '+ranking[i].socialRating+' '+ranking[i].practiceRating+' '+ranking[i].basicRating+' '+ranking[i].prizeRating);
-				arr.push(t);
-			}
-			console.log(arr.length);
-			vscode.window.showQuickPick(arr,{
-                canPickMany:false,
-                ignoreFocusOut:true,
-                matchOnDescription:true,
-                matchOnDetail:true,
-                placeHolder:body.currentTitle
-            }).then(value => {
-				vscode.window.showInformationMessage("看看人家"+ value.label+",再看看你自己！");
-			})
-		});
-
+		for (var k = 1; k <= 2; k++) {
+			var body = {};
+			getLuoguApi('https://www.luogu.com.cn/ranking?_contentOnly&page=' + k).then(result => {
+				body = result;
+				var ranking = body.currentData.rankList.result;
+				for (var i = 0, len = ranking.length; i < len; i++) {
+					var t = {};
+					t.label = ranking[i].user.name;
+					t.description = ranking[i].user.slogan;
+					t.detail = String(ranking[i].contestRating + ' ' + ranking[i].socialRating + ' ' + ranking[i].practiceRating + ' ' + ranking[i].basicRating + ' ' + ranking[i].prizeRating);
+					arr.push(t);
+				}
+			});
+			// setTimeout(function () { console.log(a) }, 3000);
+		}
+		setTimeout(function () {
+			console.log(arr);
+			vscode.window.showQuickPick(arr, {
+					canPickMany: false,
+					ignoreFocusOut: true,
+					matchOnDescription: true,
+					matchOnDetail: true,
+					placeHolder: body.currentTitle
+				}).then(value => {
+					vscode.window.showInformationMessage("看看人家" + value.label + ",再看看你自己！");
+				})
+		}, 5000);
 	});
 	context.subscriptions.push(helloWorld);
 	context.subscriptions.push(showranking);
