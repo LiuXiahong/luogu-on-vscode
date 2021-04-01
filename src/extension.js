4d66a6a9bf8fa177964bb466924047a189cccac4
LiuXiahong <15178725046@163.com>
2021-04-01 13:40:25 +0800

    updata

--- src/extension.js
+++ src/extension.js
@@ -18,27 +18,38 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	context.subscriptions.push(vscode.commands.registerCommand('luogu.ShowRankingList', function () {
+	context.subscriptions.push(vscode.commands.registerCommand('luogu.Search', function () {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World from luogu!');
-		CustomFunction.ShowRankingList(1);
-		console.log('luogu.ShowRankingList runs succesfully!');
+		vscode.window.showQuickPick(['Search Problems', 'Search Users', 'Search Problem Sets']).then(value => {
+			if(value=='Search Problems')
+			{
+				vscode.commands.executeCommand('luogu.SearchProblems');
+			}
+		});
 	}));
 	context.subscriptions.push(vscode.commands.registerCommand('luogu.SearchProblems', function () {
 		let options = { // 这个对象中所有参数都是可选参数
 			password: false, // 输入内容是否是密码
 			ignoreFocusOut: true, // 默认false，设置为true时鼠标点击别的地方输入框不会消失
-			placeHolder: '输入关键字', // 在输入框内的提示信息
-			prompt: '。。。', // 在输入框下方的提示信息
-		};
-		vscode.window.showInputBox().then(msg => {
+			placeHolder: '例:P1001', // 在输入框内的提示信息
+			prompt: '输入关键字' // 在输入框下方的提示信息
+		}
+		vscode.window.showInputBox(options).then(msg => {
 			console.log("用户输入：" + msg);
-			CustomFunction.SearchProblems(msg,1);
+			if (msg != undefined) {
+				CustomFunction.SearchProblems(msg, 1);
+			}
 		});
-
+		
 		console.log('luogu.SearchProblems runs succesfully!');
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('luogu.ShowRankingList', function () {
+		// The code you place here will be executed every time your command is executed
+	
+		// Display a message box to the user
+		// vscode.window.showInformationMessage('Hello World from luogu!');
+		CustomFunction.ShowRankingList(1);
+		console.log('luogu.ShowRankingList runs succesfully!');
 	}));
 }
 exports.activate = activate;
