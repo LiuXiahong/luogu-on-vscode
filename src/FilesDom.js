acea4779d68500fe8c778b6639bb3ae146455d96
LiuXiahong <15178725046@163.com>
2021-04-16 13:48:35 +0800

    updata

---
+++ src/FilesDom.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+function GetUserTemplate(uid) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cat Coding</title>
+    </head>
+    <body>
+        <p>${uid}</p>
+    </body>
+    </html>
+    `
+}
+exports.GetUserTemplate = GetUserTemplate;
+function GetProblemTemplate(pid) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cat Coding</title>
+    </head>
+    <body>
+        <p>${pid}</p>
+    </body>
+    </html>
+    `
+}
+exports.GetProblemTemplate = GetProblemTemplate;
