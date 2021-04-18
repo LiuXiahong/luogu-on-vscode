9b26def525e515f696b9cd41c5e49d469dfe7a0b
LiuXiahong <15178725046@163.com>
2021-04-18 14:54:07 +0800

    updata

--- src/FilesDom.js
+++ src/FilesDom.js
@@ -1,22 +1,29 @@
 const fs = require('fs');
-const luogu=require('./luogu');
-function GetUserTemplate(uid) {
+const showdown =require('showdown');
+function GetUserTemplate(uid,data) {
+    let converter = new showdown.Converter();
+    let markdown_html=converter.makeHtml(data.currentData.problem.background);
     return `
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Cat Coding</title>
-    </head>
-    <body>
-        <p>${uid}</p>
-    </body>
-    </html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script>
+        document.getElementById('background').innerHTML = '${markdown_html}';
+    </script>
+</head>
+
+<body>
+    <div id="background"></div>
+</body>
+
+</html>
     `
 }
 exports.GetUserTemplate = GetUserTemplate;
-function GetProblemTemplate(pid) {
+function GetProblemTemplate(pid,data) {
     return `
     <!DOCTYPE html>
     <html lang="en">
--- src/webview.js
+++ src/webview.js
@@ -1,13 +1,21 @@
 const vscode = require('vscode');
 const FilesDom=require('./FilesDom');
+const luogu=require('./luogu');
 async function ShowProblem(pid) {
-    const panel = vscode.window.createWebviewPanel('problem information','题目详情',vscode.ViewColumn.Two,{});
-    panel.webview.html=FilesDom.GetUserTemplate(String(pid));
+    var data=await luogu.GetLuoguApi('https://www.luogu.com.cn/problem/'+pid+'?_contentOnly',null,luogu.DoNotParse);
+    // console.log(data.currentData.problem.background);
+    const panel = vscode.window.createWebviewPanel('problem information','题目详情',vscode.ViewColumn.Two,{
+        enableScripts: true
+    });
+    panel.webview.html=FilesDom.GetUserTemplate(String(pid),data);
 }
 exports.ShowProblem = ShowProblem;
 
 async function ShowUser(uid) {
-    const panel = vscode.window.createWebviewPanel('user information','用户详情',vscode.ViewColumn.Two,{});
-    panel.webview.html=FilesDom.GetUserTemplate(String(uid));
+    var data=await luogu.GetLuoguApi('https://www.luogu.com.cn/user/'+uid+'?_contentOnly',null,luogu.DoNotParse);
+    const panel = vscode.window.createWebviewPanel('user information','用户详情',vscode.ViewColumn.Two,{
+        enableScripts: true
+    });
+    panel.webview.html=FilesDom.GetUserTemplate(String(uid),data);
 }
 exports.ShowUser = ShowUser;
